218c67383abd851222407f4edbeaf7332fe7494d
GabrielEduardoBrambilla <55161971+GabrielEduardoBrambilla@users.noreply.github.com>
2022-03-30 08:33:47 -0300

    Getting Profile Picture and Background for party

--- controller/upload.php
+++ controller/upload.php
@@ -1,37 +1,67 @@
 <?php
 include('verify_login.php');
 include('../model/conection_DB.php');
-if(isset($_POST['upload']))
-{   
-     
- $file = $_SESSION['user']."-".$_FILES['file']['name'];
-    $file_loc = $_FILES['file']['tmp_name'];
- $file_type = $_FILES['file']['type'];
+if(isset($_POST['upload_profile'])){   
+   
+   $userName = $_SESSION['user'];  
+   echo $userName;
+   $file = $userName."-".$_FILES['file']['name'];
+      $file_loc = $_FILES['file']['tmp_name'];
+   $file_type = $_FILES['file']['type'];
 
-// probably going to make an include right here;
- $folder="../uploads/";
-  
- 
-// make file name in lower case
- $lowerCase_file_name = strtolower($file);
- 
- $final_file=str_replace(' ','-',$lowerCase_file_name);
- 
- if(move_uploaded_file($file_loc,$folder.$final_file))
- {
-  $sql="INSERT INTO usuario(fotoDePerfilUsuario) VALUES('$folder$final_file')";
-  mysqli_query($conection,$sql);
-  
- 
-  echo "File sucessfully upload   ";
-  echo "$file_type";
-  
+   // probably going to make an include right here;
+   $folder="../uploads/";
+   
+   // make file name in lower case
+   $lowerCase_file_name = strtolower($file);
+   
+   $final_file=str_replace(' ','-',$lowerCase_file_name);
+   
+   if(move_uploaded_file($file_loc,$folder.$final_file))
+   {
+      
+   $sql="UPDATE usuario set fotoDePerfilUsuario = '$folder$final_file' where nomeUsuario = '$userName'  ";
+   mysqli_query($conection,$sql);
+   
+   header("Location: ../view/party_form_pictures_background.html");
+   exit();
+         
+   
  }
- else
- {
-  
-  echo "Error.Please try again";
-		
-		}
+}
+ if(isset($_POST['upload_background']))
+      {   
+      $userName = $_SESSION['user'];  
+      echo $userName;
+      $file = $userName."-".$_FILES['file']['name'];
+         $file_loc = $_FILES['file']['tmp_name'];
+      $file_type = $_FILES['file']['type'];
+
+      // probably going to make an include right here;
+      $folder="../uploads/";
+      
+      // make file name in lower case
+      $lowerCase_file_name = strtolower($file);
+      
+      $final_file=str_replace(' ','-',$lowerCase_file_name);
+      
+      if(move_uploaded_file($file_loc,$folder.$final_file))
+      {
+         
+      $sql="UPDATE usuario set fotoDePerfilUsuario = '$folder$final_file' where nomeUsuario = '$userName'  ";
+      mysqli_query($conection,$sql);
+      
+      header("Location: ../view/party_register_confimation.html");
+      exit();
+            
+      
+      }
+      else
+      {
+      
+      header("Location : ../party_form_pictures.html");
+      exit();
+            
+            }
 	}
 ?>
